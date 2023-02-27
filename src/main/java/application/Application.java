1e1e2d4144812be68783ed204b42b3fe51f8f618
liuhongyu <liuhongyu0131@163.com>
2023-02-27 09:54:56 +0800

    插入排序

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,17 +1,31 @@
 package application;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.db.Db;
+import cn.hutool.db.Entity;
+import cn.hutool.json.JSONUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lhy
- * @version 1.0.0 2022/05/07
+ * @version 1.0.0 2022/11/13
  */
 public class Application {
 
-  public static void main(String[] args) {
-    Integer a = new Integer(123);
-    Integer b = new Integer(123);
-    System.out.println(a == b);
+  public static void main(String[] args) throws Exception {
+    List<Entity> all = Db.use()
+        .findAll(Entity.create("ent_business_information_food_production").set("id", "> 10000"));
+    all.forEach(item -> {
+      Long id = item.getLong("id");
+      Long pid = item.getLong("pid");
+      String content = item.getStr("content");
+
+      List<String> resultFindAll = ReUtil.findAll("[1-9.]{2}[\\u4e00-\\u9fa5 ：， 、% （ ）]+", content, 0,
+          new ArrayList<>());
+      System.out.println(id);
+      System.out.println(content);
+      System.out.println(JSONUtil.toJsonStr(resultFindAll));
+    });
   }
 }
