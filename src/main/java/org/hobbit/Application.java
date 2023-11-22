c4615f239392a1d00e6bbbc73013c5b076dfe4db
lhy <liuhongyu0131@163.com>
2023-11-22 17:16:32 +0800

    a

--- src/main/java/org/hobbit/Application.java
+++ src/main/java/org/hobbit/Application.java
@@ -1,16 +1,12 @@
 package org.hobbit;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author lhy
  * @version 1.0.0 2023/9/19
  */
-@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+
   }
 }
